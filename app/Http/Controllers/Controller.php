af8cebf204933d680384c956a0b496b5761f43cf
ziad abo alkamar <ziad-lovelyboy@hotmail.com>
2022-07-24 18:45:24 +0300

    xi

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,6 +11,6 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function hi(){
-        return "hi";
+        return "hhhhhh";
     }
 }
